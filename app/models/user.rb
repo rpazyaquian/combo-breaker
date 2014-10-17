cfdc45369b615a38611bc880e4e61e63fdc47ae1
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-17 00:38:00 -0400

    vOverwrite Devise's find_for_database_authentication method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, :uniqueness => { :case_sensitive => false }
+
   attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
