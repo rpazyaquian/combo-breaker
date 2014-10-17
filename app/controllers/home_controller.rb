faea5e0a34b73e366f3e9606cf63714dca1ccf96
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-17 01:07:15 -0400

    added combo breaker code to controller, just for the night

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
 class HomeController < ApplicationController
   def index
   end
+
+  private
+
+    def combo_breaker
+      require 'yelp'
+
+      client = Yelp::Client.new({
+        consumer_key: ENV["YELP_CONSUMER_KEY"],
+        consumer_secret: ENV["YELP_CONSUMER_SECRET"],
+        token: ENV["YELP_ACCESS_TOKEN"],
+        token_secret: ENV["YELP_ACCESS_SECRET"]
+      })
+
+      cuisines = [:italian, :mexican, :chinese, :japanese, :indian, :pizza, :burgers]
+
+      params = { term: 'food', category_filter: cuisines.sample.to_s }
+
+      location = 'Coolidge Corner, Brookline, MA'
+
+      results = client.search(location, params)
+    end
+
 end
