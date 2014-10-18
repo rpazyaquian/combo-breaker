ab44825897aad2738458ac1c3c4eecd05f6161c4
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-18 14:58:04 -0400

    added sessions routes, updated header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   post '/search', to: 'home#search'
+  get '/sign_out', to: 'sessions#destroy', as: :sign_out
+  get '/sign_in', to: 'sessions#new', as: :sign_in
+  post '/sign_in', to: 'sessions#create', as: :login
+
 end
