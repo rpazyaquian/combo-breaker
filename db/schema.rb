f148af6c6d520993c200a4320f8c5fd13891efa4
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-20 11:40:46 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018161533) do
+ActiveRecord::Schema.define(version: 20141020153909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "display_name"
+    t.string   "search_value"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "locations", force: true do |t|
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations_categories", id: false, force: true do |t|
+    t.integer "location_id"
+    t.integer "category_id"
+  end
+
+  add_index "locations_categories", ["category_id"], name: "index_locations_categories_on_category_id", using: :btree
+  add_index "locations_categories", ["location_id"], name: "index_locations_categories_on_location_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string "email"
