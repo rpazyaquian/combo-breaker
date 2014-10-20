a297a2ff48904fc59311e042ba702faa732b8774
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-20 17:33:15 -0400

    added validations to search form"

--- app/models/search_form.rb
+++ app/models/search_form.rb
@@ -1,2 +1,13 @@
 class SearchForm < ActiveRecord::Base
+  validates :location, presence: true
+  validates :cuisine, presence: true
+  validates :radius_distance, presence: true
+  validates :radius_units, presence: true
+  validate :radius_must_be_less_than_40000_m
+
+  def radius_must_be_less_than_40000_m
+    if Unit("#{distance} #{units}").convert_to('m').scalar > 40000
+      errors.add(:radius_distance, "must be less than 40000 m")
+    end
+  end
 end
