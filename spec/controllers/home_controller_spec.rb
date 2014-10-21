b6b3f54c9f19df834d49423b858a384268001142
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-21 09:54:31 -0400

    created skeleton search class, updated controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
+  describe "#search" do
+    :post, search_form: {
+      location: 'Coolidge Corner, Brookline, MA',
+      cuisine: :burgers,
+      radius_distance: 1,
+      radius_units: :mi
+    }
+    expect(response.status).to eq(200)
+  end
 end
