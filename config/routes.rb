1150152937876faffd45d979ac0c8800f5978a18
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-22 14:48:14 -0400

    added redo route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get '/edit_profile', to: 'users#edit', as: :edit_profile
   post '/search', to: 'home#search'
+  post '/redo', to: 'home#redo'
   get '/sign_out', to: 'sessions#destroy', as: :sign_out
   get '/sign_in', to: 'sessions#new', as: :sign_in
   post '/sign_in', to: 'sessions#create', as: :login
